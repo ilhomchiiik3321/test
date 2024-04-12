d39cea3e3251cf5fbccaae682b4b94caceefbd29
ilhomchiiik3321 <162345711+ilhomchiiik3321@users.noreply.github.com>
2024-04-12 16:25:37 +0500

    Create test_bot.py
    VERSIYA 1

---
+++ test_bot.py
@@ -0,0 +1,10 @@
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    user_info = get_info(message)
+    store_user_data(message.chat.id, user_info)
+    markup=types.InlineKeyboardMarkup()
+    markup.row(types.InlineKeyboardButton('GBP', callback_data='GBP'),
+               types.InlineKeyboardButton('USD', callback_data='USD'),
+               types.InlineKeyboardButton('RUB', callback_data='RUB'))
+    bot.send_message(message.chat.id, "Choose a currency:", reply_markup = markup)
